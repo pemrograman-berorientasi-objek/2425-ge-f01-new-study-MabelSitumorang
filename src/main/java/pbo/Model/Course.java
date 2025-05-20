dbfdd3e047a4785b32f843e6d2a4fa0ec1f33af0
MabelSitumorang <situmorangmabel@gmail.com>
2025-05-20 16:22:34 +0700

    Test 100

---
+++ src/main/java/pbo/Model/Course.java
@@ -0,0 +1,53 @@
+package pbo;
+package pbo.model;
+
+import java.util.*;
+import javax.persistence.*;
+
+/**
+ * Mabel Christoffel A.S - 12S23011
+ * Amos Manurung - 12S23027
+ *
+ */
+@Entity
+@Table(name = "Course")
+public class Course {
+  @Id
+  @Column(name = "idCourse", nullable = false, length = 100)
+  private String idCourse;
+
+  @Column(name = "courseName", nullable = false, length = 100)
+  private String courseName;
+
+  @Column(name = "semester", nullable = false, length = 100)
+  private String semester;
+
+  @Column(name = "kredit", nullable = false, length = 100)
+  private String kredit;
+
+  public Course() {
+  }
+
+  public Course(String idCourse, String courseName, String semester, String kredit) {
+    this.idCourse = idCourse;
+    this.courseName = courseName;
+    this.semester = semester;
+    this.kredit = kredit;
+  }
+
+  public String getIdCourse() {
+    return this.idCourse;
+  }
+
+  public String getCourseName() {
+    return this.courseName;
+  }
+
+  public String getSemester() {
+    return this.semester;
+  }
+
+  public String getKredit() {
+    return this.kredit;
+  }
+}
---
+++ src/main/java/pbo/Model/Student.java
@@ -0,0 +1,45 @@
+package pbo;
+import java.util.*;
+import javax.persistence.*;
+
+/**
+ * Mabel Christoffel A.S - 12S23011
+ * Amos Manurung - 12S23027
+ *
+ */
+
+@Entity
+@Table(name = "Student")
+public class Student {
+
+  @Id
+  @Column(name = "NIM", nullable = false, length = 100)
+  private String NIM;
+
+  @Column(name = "nama", nullable = false, length = 100)
+  private String nama;
+
+  @Column(name = "prodi", nullable = false, length = 100)
+  private String prodi;
+
+  public Student() {
+  }
+
+  public Student(String NIM, String nama, String prodi) {
+    this.NIM = NIM;
+    this.nama = nama;
+    this.prodi = prodi;
+  }
+
+  public String getNama() {
+    return this.nama;
+  }
+
+  public String getNIM() {
+    return this.NIM;
+  }
+
+  public String getProdi() {
+    return this.prodi;
+  }
+}
